f3e9a3575a6ad3ed84fe989014f6878eeeefc80a
aaditi-code <patilaaditi2116@gmail.com>
2026-01-11 16:44:36 +0530

    Second Commit

--- demo/src/main/java/com/vbs/demo/controller/HistoryController.java
+++ demo/src/main/java/com/vbs/demo/controller/HistoryController.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class HistoryController {
     @Autowired
     HistoryRepo historyRepo;
+
     @GetMapping("/histories")
     public List<History> getHistories() {
         return historyRepo.findAll();
